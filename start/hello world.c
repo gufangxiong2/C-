e62e89aca504e4982203462d142dfd948a4538fa
gufangxiong2 <152645385+gufangxiong2@users.noreply.github.com>
2024-12-23 17:55:55 +0800

    Update hello world.c

--- start/hello world.c
+++ start/hello world.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
-int main()
+#include <stdio.h> //include预处理引出头文件 stdio.h则是包含着标准输入输出的头文件
+int main() //代码从main处开始运行
 {
-    printf("hello world");
-    return 0;
+    printf("hello world");//print("....")表示打印...
+    return 0;//用于退出程序
 }
